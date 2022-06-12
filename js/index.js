a23bcfe09d48a4214c5491fc75fc749f6d2706e3
Robert Sainton <robertsainton@outlook.com>
2022-06-12 01:43:52 -0400

    deliverables completed

--- js/index.js
+++ js/index.js
@@ -4,29 +4,149 @@ const init = () => {
         event.preventDefault();
         const input = document.querySelector('input#search');
         console.log(input.value);
-         fetch(`https://api.github.com/search/users?q=${input.value}`,{
+        const ghApiUrl = `https://api.github.com/users/${input.value}`
+        console.log(ghApiUrl)
+         fetch(ghApiUrl,{
            
             method: "GET",
             headers: {
               Accept: "application/vnd.github.v3+json" ,
               
             }
-        })
-    .then(response => response.json())
-    .then(response => {
-         document.getElementById('user-list').innerHTML = response;
-      //  response.forEach(()=>{
-        //    const li = document.createElement('li')
-          //  list.appendChild(li) 
+        },{
+            username: 'USERNAME'
+          })
+    .then((resp) => resp.json())
+    .then((json) => {
+        console.log(json)
+        const text = json
+        console.log(text.login)
+        const loginName = text.login
+        const avatarUrl = text.avatar_url
+       const profileLink = text.html_url
+        const firstMerge = loginName.concat(avatarUrl)
+        const finalMerge = firstMerge.concat(profileLink)
+        console.log(finalMerge)
+      //  const title = document.querySelector('section#movieDetails h4');
+        //const summary = document.querySelector('section#movieDetails p');
+        //title.innerText = data.title;
+  //  summary.innerText = data.summary;
+  document.getElementById('user-list');
+
+ display(loginName);
+
+ const img = document.createElement('img');
+  img.src = avatarUrl ;
+ // img.title = "avatar"
+//'https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png';
+  document.getElementById('user-list').appendChild(img);
+
+
+  
+ // const link = document.createTextNode(profileLink)
+  ///li.appendChild(link)
+ // li.title = "Profile"
+ // li.href = text.html_url
+ // document.getElementById('user-list').appendChild(li)
+
+ display (profileLink);
+  
+ 
+   const el = document.getElementById("github-container");
+    el.addEventListener('click', () =>{
+    console.log("Hello");
+    const repoApiUrl = `https://api.github.com/users/${input.value}/repos`
+    console.log(repoApiUrl)
+     fetch(repoApiUrl,{
+       
+        method: "GET",
+        headers: {
+          Accept: "application/vnd.github.v3+json" ,
+          
+        }
+    })
+.then((resp) => resp.json())
+.then((json) => {
+    console.log(json)
+    const repoData = json
+    repoData.forEach(displayRepo);
+   
+})
+   } )
+ //  el.onclick = getRepos(loginName);
+
+
+
+ 
+ // cdisplayLink (profileLink);
         
-      //  })
+         
+   
+
+// const obj1 =JSON.stringify(json)
+      ///  const deStringifiedUser = JSON.parse('')
+
+     //console.log(deStringifiedUser)
+  
+
+
+     //   Object.entries(json).forEach(entry => {
+       //     let key = entry[0];
+         //   let value = entry[1];
+           // console.log(key, value);
+
+          
+          });
+          
+
+
+       
+          
+    })
+    }
+    
+             //  function display(json) {
+
+      
+        //user.forEach(data=>{
+         //  const li = document.createElement('li')
+         //  li.innerHTML = data.login
+         //  list.appendChild(li) 
+        
+     //   })
+  //  }
        
       // list.innerText = data.list;
     
-   // console.log(li)
-    console.log(response)
-});
-});
-}
+   
+   // console.log(response)
+//}
+
+//function displayavatar (){
+ /////   console.log(avatarUrl)
+    
+//}
+
+  //  function diplayName (){
+    
+///}
+
+  /////  function displayLink (){
+    
+//}
+
+function display (data) {
+ 
+    let p = document.createElement('p');
+    p.textContent = data;
+    document.querySelector('#user-list').appendChild(p);
+  }
+  function displayRepo (repoData) {
+    document.getElementById('repos-list');
+    let p = document.createElement('p');
+ p.textContent = `${repoData.name}`;
+ document.querySelector('#repos-list').appendChild(p);
+  }
+ 
 
 document.addEventListener('DOMContentLoaded', init);
